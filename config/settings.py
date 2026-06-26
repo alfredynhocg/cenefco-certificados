1027fcd4509a4c8d4599dc5bc234651bcfeee76c
alfredynho <alfredynho.cg@gmail.com>
2026-06-26 15:24:15 -0400

    Agregar CSRF_TRUSTED_ORIGINS configurable para acceso por IP sin HTTPS
    Sin esto, Django 4+ rechaza los POST (login, formularios) con 403 al
    acceder por IP:puerto en sitios sin HTTPS detras de un proxy.

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,15 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 
+# Necesario para que los formularios (login, crear lote, etc) funcionen al
+# acceder por IP:puerto sin HTTPS. Cuando se configure un dominio con SSL,
+# agregar aqui tambien el origen https://tu-dominio en ALLOWED_HOSTS/.env.
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    default="http://127.0.0.1:8001",
+    cast=Csv(),
+)
+
 
 # Application definition
 
